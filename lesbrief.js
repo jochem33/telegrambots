a3a3d2277cc8bdfb47d2ff5425d6f8f13a6d01f0
jochem33 <jochemjuriaan.baas@coderclass.nl>
2017-12-19 10:30:09 +0100

    [add] random func toegevoegd

--- lesbrief.js
+++ lesbrief.js
@@ -4,19 +4,37 @@ const TeleBot = require('telebot');
 const bot = new TeleBot('508978429:AAHcNczEHomrFMLKGmF23WpDbfwy1WBQQs4');
 
 bot.on('/start', function (msg) {
-  return bot.sendMessage(msg.from.id, "Hello world!");
+  console.log(msg.from.first_name + " " + msg.from.last_name + " zei /start");
+  return bot.sendMessage(msg.from.id, "Hallo " + msg.from.first_name);
 });
 
 bot.on('/wiebenje', function (msg) {
+  console.log(msg.from.first_name + " " + msg.from.last_name + " zei /wiebenje");
   return bot.sendMessage(msg.from.id, "Hoi, ik ben Hakan", {replyToMessage: msg.message_id, notification: false});
 });
 
 bot.on('/foto', function (msg) {
+  console.log(msg.from.first_name + " " + msg.from.last_name + " zei /foto");
   return bot.sendPhoto(msg.from.id, "images/splash.jpg");
 });
 
 bot.on('/versie', function (msg) {
+  console.log(msg.from.first_name + " " + msg.from.last_name + " zei /versie");
   return bot.sendMessage(msg.from.id, "Ik ben momenteel op versie 1.0");
 });
+
+bot.on(/^([hH][oa]i|[Hh]allo!*)/, function (msg) {
+  console.log(msg.from.first_name + " " + msg.from.last_name + " zei hoi");
+  return bot.sendMessage(msg.from.id, "Hallo " + msg.from.first_name);
+});
+
+bot.on(/^[Dd]oei/, function (msg) {
+  console.log(msg.from.first_name + " " + msg.from.last_name + " zei doei");
+  return bot.sendMessage(msg.from.id, "Doei " + msg.from.first_name);
+});
+
+function random(aantal) {
+  return Math.floor(Math.random() * aantal);
+}
 
 bot.start();
