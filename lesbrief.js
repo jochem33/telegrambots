d886b7a37d08cee1180d81f157879bafa017db3d
jochem33 <jochemjuriaan.baas@coderclass.nl>
2018-01-16 13:27:44 +0100

    [change] antwoord wordt nu gegeven in 1 eindfunctie
    [add] hoe gaat het
    [change] antwoord techniek
    [remove] -

--- lesbrief.js
+++ lesbrief.js
@@ -5,6 +5,7 @@ const TeleBot = require('telebot');
 
 const bot = new TeleBot('508978429:AAHcNczEHomrFMLKGmF23WpDbfwy1WBQQs4');
 
+var answer;
 var lastmsg;
 
 var leeftijd;
@@ -21,11 +22,11 @@ if (uur < 13 || uur > 4) {
 }
 
 bot.on('/start', function (msg) {
-  return bot.sendMessage(msg.from.id, "Hallo " + msg.from.first_name);
+  answer = "Hallo " + msg.from.first_name;
 });
 
 bot.on('/wiebenje', function (msg) {
-  return bot.sendMessage(msg.from.id, "Hoi, ik ben een telegram bot met de naam Jochembot!", {replyToMessage: msg.message_id, notification: false});
+  answer = "Hoi, ik ben een telegram bot met de naam Jochembot!";
 });
 
 bot.on('/foto', function (msg) {
@@ -33,7 +34,26 @@ bot.on('/foto', function (msg) {
 });
 
 bot.on('/versie', function (msg) {
-  return bot.sendMessage(msg.from.id, "Ik ben momenteel op versie 1.0");
+  answer = "Ik ben momenteel op versie 2.0";
+});
+
+bot.on(/^([hH][oai]+i|[Hh]allo!*|[Gg]oe[id]emorgen!*|[Hh]i+)/, function (msg) {
+  var hoi = ["Hoi", "Hallo", "Goedendag", goedemorgen, "Hoi " + msg.from.first_name, "Hallo " + msg.from.first_name, "Goedendag " + msg.from.first_name, goedemorgen  + msg.from.first_name]
+  answer = hoi[random(8)];
+});
+
+bot.on(/^[Dd]oei/, function (msg) {
+  answer = "Doei " + msg.from.first_name;
+});
+
+bot.on(/^([Hh]oe.*\?+|[Ww]a+rom.*\?+)/, function (msg, props) {
+  var ans = ["Ik weet het niet", "Ik begrijp de vraag niet zo goed", "??", "Ik heb geen idee"]
+  answer = ans[random(4)];
+});
+
+bot.on(/[Hh]oe is het\??|[Hh][Gg][Hh]\??|[Hh]oe gaat het\?? ?e?r? ?m?e?e?\??|[Aa]lles goed\??/, function (msg) {
+  var hoi = ["Goed", "Prima", "Normaal", "Z'n gangetje", "Goed, en met jou?", "Prima, en met jou?", "Z'n gengetje, en jij?"]
+  answer = hoi[random(8)];
 });
 
 bot.on(/\/tijd|[Hh]oe laat is het ?n?u?[\?]?/, function (msg) {
@@ -41,28 +61,14 @@ bot.on(/\/tijd|[Hh]oe laat is het ?n?u?[\?]?/, function (msg) {
   if (uren > 12) {
     uren -= 12;
   }
-  return bot.sendMessage(msg.from.id, "Het is nu " + date.getMinutes() + " over " + uren);
-});
-
-bot.on(/^([hH][oai]+i|[Hh]allo!*|[Gg]oe[id]emorgen!*|[Hh]i+)/, function (msg) {
-  var hoi = ["Hoi", "Hallo", "Goedendag", goedemorgen, "Hoi " + msg.from.first_name, "Hallo " + msg.from.first_name, "Goedendag " + msg.from.first_name, goedemorgen  + msg.from.first_name]
-  return bot.sendMessage(msg.from.id, hoi[random(8)]);
-});
-
-bot.on(/^[Dd]oei/, function (msg) {
-  return bot.sendMessage(msg.from.id, "Doei " + msg.from.first_name);
-});
-
-bot.on(/^([Hh]oe.*\?+|[Ww]a+rom.*\?+)/, function (msg) {
-  var ans = ["Ik weet het niet", "Ik begrijp de vraag niet zo goed", "??", "Ik heb geen idee"]
-  return bot.sendMessage(msg.from.id, ans[random(4)]);
+  answer = "Het is nu " + date.getMinutes() + " over " + uren;
 });
 
 bot.on(/[Ii]k ben ([0-9][0-9])/, function (msg, props) {
   var leeftijd = props.match[1];
   var ans = ["Ok", "cool", "Oké, ik ben nog niet zo oud."];
   senddata(msg.from.first_name, leeftijd[7] + leeftijd[8])
-  return bot.sendMessage(msg.from.id, ans[random(3)]);
+  answer = ans[random(3)];
 });
 
 bot.on(/[Hh]oe oud ben ik\??/, function (msg) {
@@ -71,13 +77,17 @@ bot.on(/[Hh]oe oud ben ik\??/, function (msg) {
   //leeftijd = getdata(msg.from.first_name);
   setTimeout(function(){
     console.log("in regex leeftijd is: " + leeftijd);
-    return bot.sendMessage(msg.from.id, "je bent " + leeftijd + " jaar oud.");
-  }, 300);
+    answer = "je bent " + leeftijd + " jaar oud.";
+  }, 200);
 });
 
 bot.on(/(.+)/, function (msg, props) {
   lastmsg = props.match[1];
-  console.log(msg.from.first_name + " " + msg.from.last_name + ": " + lastmsg);
+  setTimeout(function(){
+    console.log(msg.from.first_name + " " + msg.from.last_name + ": " + lastmsg);
+    console.log("Bot: " + answer)
+    return bot.sendMessage(msg.from.id, answer);
+  }, 300);
 });
 
 
