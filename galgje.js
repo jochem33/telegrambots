182f684d283cf6d8e0629f4f5513a7d3db168cac
jochem33 <jochemjuriaan.baas@coderclass.nl>
2018-02-14 12:56:36 +0100

    [add] debugging voor de call error

--- galgje.js
+++ galgje.js
@@ -5,8 +5,8 @@ const TeleBot = require('telebot');
 
 const bot = new TeleBot('508978429:AAHcNczEHomrFMLKGmF23WpDbfwy1WBQQs4');
 
-var answer;
-var lastmsg;
+var answer = "Knibbel Knabbel Knuisje";
+var answer2 = voortgang();
 var lastlastmsg;
 var ansid;
 var lastansid;
@@ -15,12 +15,57 @@ var leeftijd;
 
 woordenlijst = ["ijs", "pizza", "telegram"];
 
-woord = woordenlijst[random(3)];
-geradenletters = [];
-foutgeradenletters= [];
-aantalletters = woord.lenght;
+var woord = woordenlijst[random(3)];
+var geradenletter;
+var geradenletters = [];
+var foutgeradenletters = [];
+var foutgeraden = 0;
+var aantalletters = woord.lenght;
 for (i = 0; i < aantalletters; i++) {
   geradenletters.push("_");
+}
+
+function voortgang() {
+  if (foutgeraden == 0){
+    answer = "________";
+  }
+  if (foutgeraden == 1){
+    answer = " |\n |\n |\n |\n |\n |\n________";
+  }
+  if (foutgeraden == 2){
+    answer = "________\n |/\n |\n |\n |\n |\n |\n________";
+  }
+  if (foutgeraden == 3){
+    answer = "________" +
+            "\n |/" +
+            "\n |" +
+            "\n |" +
+            "\n |" +
+            "\n |" +
+            "\n |" +
+            "\n________";
+  }
+  if (foutgeraden == 4){
+    answer = "________" +
+            "\n |/     |" +
+            "\n |     ( )" +
+            "\n |" +
+            "\n |" +
+            "\n |" +
+            "\n |" +
+            "\n________";
+  }
+  if (foutgeraden == 5){
+    answer = "________" +
+            "\n |/     |" +
+            "\n |     ( )" +
+            "\n |     /|\\" +
+            "\n |     / \\" +
+            "\n |" +
+            "\n |" +
+            "\n________";
+  }
+  return(answer);
 }
 
 bot.on('/start', function (msg) {
@@ -28,20 +73,23 @@ bot.on('/start', function (msg) {
   return bot.sendMessage(msg.from.id, "Hallo " + msg.from.first_name + " welkom bij galgje");
 });
 
-bot.on(/^([a-zA-Z])$/), function (msg, props) {
-  geradenletter = props.match(1);
+
+bot.on(/[aA]//*^([a-zA-Z])$*/), function (msg, props) {
+  geradenletter = "h"; //props.match(1);
   var woordinletter = woord.indexOf(geradenletter);
-  if(woordinletter != -1){
+  if(woordinletter >= 0){
     geradenletters[woordinletter] = geradenletter;
-    answer = "goedzo, je hebt een letter geraden!";
+    answer = "Goedzo, je hebt een letter geraden!";
   } else {
-    answer = "jammer, probeer het nog een keer";
+    foutgeradenletters.push(geradenletter);
+    foutgeraden++;
+    answer = "Jammer, probeer het nog een keer";
+    answer2 = voortgang();
   }
+  return bot.sendMessage(msg.from.id, "H ");
 }
 
 bot.on(/(.+)/, function (msg, props) {
-  lastansid = ansid;
-  lastlastmsg = lastmsg;
   lastmsg = props.match[1];
   setTimeout(function(){
     console.log(msg.from.first_name + " " + msg.from.last_name + ": " + lastmsg);
@@ -49,6 +97,10 @@ bot.on(/(.+)/, function (msg, props) {
     return bot.sendMessage(msg.from.id, answer);
   }, 300);
   //sendconversation(msg.from.first_name + " " + msg.from.last_name, msg.from.id, answer, lastmsg);
+});
+
+bot.on(/(.+)/, function (msg, props) {
+  return bot.sendMessage(msg.from.id, answer2)
 });
 
 
